0d2f284381de3de9181015e56fbd3a1e9a80d453
idanfadlon <66566962+idanfadlon@users.noreply.github.com>
2023-02-18 15:40:54 +0200

    convert all code to type script

---
+++ src/routes/post_routes.ts
@@ -0,0 +1,13 @@
+import express from 'express'
+const router = express.Router()
+import Post from '../controllers/post'
+
+router.get('/',Post.getAllPosts)
+
+router.get('/:id',Post.getPostById)
+
+router.post('/', Post.addNewPost)
+
+router.put('/:id',Post.updatePostById)
+
+export= router
